aed577e5cb81f8efd67683c8994a5e3a163a5036
Joseph Farrish <joseph_a_f@hotmail.com>
2020-09-25 08:59:06 -0700

    setting api routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -2,12 +2,25 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const wo = require("../models/wrkout.js");
 
+//from api.js getLastworkout()
 router.get("/api/workouts", (req, res)=>{
-    wo.find({}).sort({dat: 1}).then(woDb =>{
+    wo.find({}).sort({day: 1}).then(woDb =>{
     }).catch(err=>{
         res.status(400).json(err);
     });
 });
+//from api.js addExercise()
+router.put("/api/workouts/", ({body}, res)=>{
+    wo.create(body).then(woDb =>{
+        res.json(woDb);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+
+
+
 
 
 
